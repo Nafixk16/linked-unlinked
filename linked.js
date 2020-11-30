2443c7b47cfac0e6ed8d9a322d99f85d8b918756
Nafix16 <40899454+Nafix16@users.noreply.github.com>
2020-11-30 22:47:12 +0500

    completed task

--- linked.js
+++ linked.js
@@ -1,6 +1,7 @@
 // find all the users that have not been linked
 import { allUsers, linked } from "./users";
-function findUnlinkeduser() {
+
+function findUnlinkeduser(allUsers, linked) {
   let unlinkedList = [];
   for (let i = 0; i < allUsers.length; i++) {
     const found = linked.some((user) => user.name === allUsers[i].name);
@@ -8,6 +9,7 @@ function findUnlinkeduser() {
       unlinkedList.push(allUsers[i]);
     }
   }
+  return unlinkedList;
 }
 
-findUnlinkeduser();
+findUnlinkeduser(allUsers, linked);
